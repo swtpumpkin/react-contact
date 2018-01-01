abe0267206af0c06eddc742d90f1046dc5fe6004
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-01 20:56:36 +0900

    Immutability Helper added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
 
@@ -26,6 +27,10 @@ export default class Contact extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleChange(e) {
@@ -41,6 +46,34 @@ export default class Contact extends React.Component {
     console.log(key, "is selected")
   }
 
+  handleCreate(contact) {
+    this.setState({
+      contactData: update(this.state.contactData, { $push: [contact] })
+    });
+  }
+
+  handleRemove() {
+    this.setState({
+      contactData: update(this.state.contactData,
+        { $splice: [[this.state, selectedKey, 1]] }
+      ),
+      selectedKey: -1
+    });
+  }
+
+  handleEdit(name, phone) {
+    this.setState({
+      contactData: update(this.state.contactData,
+        {
+          [this.state.selectedKey]: {
+            name: { $set: name },
+            phone: { $set: phone }
+          }
+        }
+      )
+    })
+  }
+
   render() {
     const mapToComponents = (data) => {
       data.sort();
@@ -54,14 +87,14 @@ export default class Contact extends React.Component {
           contact={contact}
           key={i}
           onClick={() => this.handleClick(i)}
-          />);
+        />);
       });
     };
 
     return (
       <div>
         <h1>Contacts</h1>
-        <input 
+        <input
           name="keyword"
           placeholder="Search"
           value={this.state.keyword}
@@ -69,8 +102,8 @@ export default class Contact extends React.Component {
         />
         <div>{mapToComponents(this.state.contactData)}</div>
         <ContactDetails
-        isSelected={this.state.selectedKey != -1}
-        contact={this.state.contactData[this.state.selectedKey] }
+          isSelected={this.state.selectedKey != -1}
+          contact={this.state.contactData[this.state.selectedKey]}
         />
       </div>
     );
