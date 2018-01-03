bd0aa1eea08add3e3b98a5d6e6edaed02bbd8897
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-03 13:31:47 +0900

    create Revise, Remove function.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -55,9 +55,12 @@ export default class Contact extends React.Component {
   }
 
   handleRemove() {
+    if(this.state.selectedKey < 0){
+      return;
+    }
     this.setState({
       contactData: update(this.state.contactData,
-        { $splice: [[this.state, selectedKey, 1]] }
+        { $splice: [[this.state.selectedKey, 1]] }
       ),
       selectedKey: -1
     });
@@ -106,6 +109,8 @@ export default class Contact extends React.Component {
         <ContactDetails
           isSelected={this.state.selectedKey != -1}
           contact={this.state.contactData[this.state.selectedKey]}
+          onRemove={this.handleRemove}
+          onEdit={this.handleEdit}
         />
         <ContactCreate
           onCreate={this.handleCreate}
