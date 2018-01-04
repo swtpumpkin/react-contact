d863009ea5a715c6b58bdbd348b0b5f4fb5f0e6c
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-05 06:59:16 +0900

    create Keypress & ref function.

--- src/components/ContactCreate.js
+++ src/components/ContactCreate.js
@@ -10,6 +10,7 @@ export default class ContactCreate extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   handleChange(e) {
@@ -29,6 +30,13 @@ export default class ContactCreate extends React.Component {
       name:'',
       phone:''
     });
+    this.nameInput.focus();
+  }
+
+  handleKeyPress(e) {
+    if(e.charCode === 13) {
+      this.handleClick();
+    }
   }
 
   render() {
@@ -42,6 +50,7 @@ export default class ContactCreate extends React.Component {
             placeholder="name"
             value={this.state.name}
             onChange={this.handleChange}
+            ref = {(ref) => { this.nameInput = ref }}
           />
           <input
             type="text"
@@ -49,6 +58,7 @@ export default class ContactCreate extends React.Component {
             placeholder="phone"
             value={this.state.phone}
             onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
           />
           <button onClick={this.handleClick}>Create</button>
         </p>
